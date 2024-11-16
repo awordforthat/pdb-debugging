2e5634777b15a42a573ed875dc05bdc95f02e6df
Emily Charles <emily.w.charles@gmail.com>
2024-11-16 15:43:58 -0500

    cleanup, rewriting quiz prompts

--- 02_stack_navigation.py
+++ 02_stack_navigation.py
@@ -32,11 +32,11 @@ def get_question(category):
 
 
 ### Task 1a - stop the code right before `question` is assigned. What is the current context?
-### Task 1b - step into the function and list the context. How is this behavior different from n(ext)?
-### Task 1c - rerun the example and this time s(tep) six times. What function do we end up in?
+### Task 1b - step into the function using s(tep). What is the next line to be executed?
+### Task 1c - rerun the example and this time go to the n(ext) line. What is the next line to be executed?
 
-### Task 2 - Stop the code in the same place as before. Using s(tep), see if you can follow the code execution
-# through all of the function calls that happen as a result of line 42 (this is similar to 1c, just in more detail).
+### Task 2 - Stop the code in the same place as before. S(tep) into the get_question function and
+# enter n(ext) to get to line 30. S(tep) again. What function do we end up in?
 
 ### Task 3a - Stop the code in the same place as before. Print out w(here) you are. How many frames are in the stack?
 ### Task 3b - Rerun the example and step into the next function. Use the w(here) command again. How many frames are there now?
